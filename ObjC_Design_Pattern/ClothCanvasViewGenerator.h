2fdb44cfe40c2373cad692ef1ae8a0af7be446a7
manuqiao <manuqiao@kangdandeMacBook-Air.local>
2014-08-17 14:40:13 +0800

    57

---
+++ ObjC_Design_Pattern/ClothCanvasViewGenerator.h
@@ -0,0 +1,15 @@
+//
+//  ClothCanvasViewGenerator.h
+//  ObjC_Design_Pattern
+//
+//  Created by ManuQiao on 14-8-17.
+//  Copyright (c) 2014年 manu. All rights reserved.
+//
+
+#import "CanvasViewGenerator.h"
+
+@interface ClothCanvasViewGenerator : CanvasViewGenerator
+
+- (CanvasView *)canvasViewWithFrame:(CGRect)aFrame
+
+@end
